8562c68df361b23bfcc490f0b3dce4cb33902449
pbesra <psbesra@gmail.com>
2022-01-16 11:43:00 +0530

    files added

--- src/components/Example/Example.test.js
+++ src/components/Example/Example.test.js
@@ -1,5 +1,16 @@
-import { shallow } from "enzyme";
+import { configure, shallow } from "enzyme";
+import Example from "./Example";
+
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+configure({adapter: new Adapter()});
 
 describe("<Example/>", () => {
-  test("renders correctly", () => {});
+  test("renders correctly", () => {
+      const wrapper = shallow(<Example/>);
+      expect(wrapper).toBeDefined();
+  });
+  describe("test textfield", ()=>{
+    test("textfield", ()=>{});
+  })
 });
