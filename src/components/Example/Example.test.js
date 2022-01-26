3999704af33f43cc8a231403abe5bb531c84b23e
pbesra <psbesra@gmail.com>
2022-01-26 18:40:40 +0530

    test cases updated

--- src/components/Example/Example.test.js
+++ src/components/Example/Example.test.js
@@ -1,16 +1,20 @@
 import { configure, shallow } from "enzyme";
 import Example from "./Example";
+import sinon from "sinon";
 
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 configure({adapter: new Adapter()});
 
 describe("<Example/>", () => {
-  test("renders correctly", () => {
-      const wrapper = shallow(<Example/>);
-      expect(wrapper).toBeDefined();
-  });
-  describe("test textfield", ()=>{
-    test("textfield", ()=>{});
-  })
+    test("renders correctly", () => {
+        const wrapper = shallow(<Example/>);
+        expect(wrapper).toBeDefined();
+    });
+    describe("test textfield", ()=>{
+        const onChangeMock=jest.fn();
+        const event={
+            preventDefault();
+        }
+    })
 });
